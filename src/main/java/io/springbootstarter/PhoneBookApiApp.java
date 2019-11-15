3a23abcd241926c9b19a188ddc1b6165bafe044e
Tunç Gümüşdağ <tuncgumusdag@gmail.com>
2019-11-15 19:38:28 +0300

    Added a working Spring Framework in Java using Intellij IDEA

---
+++ src/main/java/io/springbootstarter/PhoneBookApiApp.java
@@ -0,0 +1,11 @@
+package io.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApiApp {
+    public static void main(String[] args){
+        SpringApplication.run(PhoneBookApiApp.class, args);
+    }
+}
