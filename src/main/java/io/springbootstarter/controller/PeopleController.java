5804334ac9d14d4cdd4a6c596196180757f7cbb4
Tunç Gümüşdağ <tuncgumusdag@gmail.com>
2019-11-19 15:01:46 +0300

    Changed package structure

---
+++ src/main/java/io/springbootstarter/controller/PeopleController.java
@@ -0,0 +1,32 @@
+package io.springbootstarter.controller;
+
+import io.springbootstarter.people.Person;
+import io.springbootstarter.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PeopleController {
+
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping("/people")
+    public List<Person> getPeopleList(){
+        return personService.getPeopleList();
+    }
+
+    @RequestMapping("/people/{id}")
+    public Person getPerson(@PathVariable String id){
+        return personService.getPerson( id );
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value="/people")
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+
+}
